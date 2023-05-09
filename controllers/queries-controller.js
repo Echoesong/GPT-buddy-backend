a4d42460fe1347021f459e87770fcf1d61c8b57e
Joseph Felker <jayefelker@gmail.com>
2023-05-09 13:26:03 -0500

    complete Create route

--- controllers/queries-controller.js
+++ controllers/queries-controller.js
@@ -1,27 +1,29 @@
-const express = require('express')
-const { Query } = require('../models/Query')
+const express = require("express");
+const { Query } = require("../models");
 
-async function index(req, res, next){
-    res.status(200).json({message: "hitting queries index"})
-} 
-
-async function create(req, res, next){
-    res.status(200).json({message: "hitting queries create"})
-} 
-
-async function detail(req, res, next){
-    res.status(200).json({message: "hitting queries detail"})
-} 
-
-async function destroy(req, res, next){
-    res.status(200).json({message: "hitting queries delete"})
-} 
-
-
-	
-module.exports = {
-    create,
-    index,
-    getOne: detail,
-    delete: destroy
+async function index(req, res, next) {
+  res.status(200).json({ message: "hitting queries index" });
 }
+
+async function create(req, res, next) {
+  try {
+    res.status(201).json(await Query.create(req.body));
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+}
+
+async function detail(req, res, next) {
+  res.status(200).json({ message: "hitting queries detail" });
+}
+
+async function destroy(req, res, next) {
+  res.status(200).json({ message: "hitting queries delete" });
+}
+
+module.exports = {
+  create,
+  index,
+  getOne: detail,
+  delete: destroy,
+};
