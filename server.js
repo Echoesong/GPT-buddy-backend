9acedade12ce3b7b9f4aa1ca077099eb8f670227
Joseph Felker <jayefelker@gmail.com>
2023-05-10 08:51:43 -0500

    set up first part of auth0

--- server.js
+++ server.js
@@ -8,12 +8,31 @@ const queriesRouter = require('./routes/queries-router.js')
 const cors = require("cors")
 const morgan = require("morgan")
 
-const { PORT } = process.env;
+const { PORT, AUTH0_SECRET, AUTH0_CLIENTID, AUTH0_BASEURL} = process.env;
+
 const app = express();
 
 ///////////////////////////////
 // MiddleWare
 ////////////////////////////////
+
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: AUTH0_SECRET,
+  baseURL: 'http://localhost:4000',
+  clientID: AUTH0_CLIENTID,
+  issuerBaseURL: AUTH0_BASEURL
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+
+// req.isAuthenticated is provided from the auth router
+
+
+app.use(auth(config));
 app.use(express.urlencoded({extended:true}))
 app.use(express.json()); // parse json bodies
 app.use(cors()); // to prevent cors errors, open access to all origins
@@ -23,9 +42,10 @@ app.use(morgan("dev")); // logging for development
 // ROUTES
 ////////////////////////////////
 // create a test route
-app.get("/", (req, res) => {
-    res.send("Hitting root");
+app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 });
+
 
 app.use('/queries', queriesRouter)
 
